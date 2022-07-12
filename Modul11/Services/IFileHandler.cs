a8ecd7e70ce8f03f1ced65c57fd9aaac4bece524
DimVinich <DmitrijVS@gmail.com>
2022-07-12 19:55:28 +0300

    перенос на комп

---
+++ Modul11/Services/IFileHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Modul11.Services
+{
+    public interface IFileHandler
+    {
+        Task Download(string fileId, CancellationToken ct);
+        string Process(string param);
+    }
+}
